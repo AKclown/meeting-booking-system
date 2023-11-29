6d2e4436e8c2059160f500e571d82f95b8a33879
zhongwenkai <13413629530@139.com>
2023-11-29 15:00:27 +0800

    feat: 添加会议室模块swagger文档生成

--- packages/meeting_room_booking_system_backend/src/meeting-room/dto/update-meeting-room.dto.ts
+++ packages/meeting_room_booking_system_backend/src/meeting-room/dto/update-meeting-room.dto.ts
@@ -1,8 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import { CreateMeetingRoomDto } from './create-meeting-room.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateMeetingRoomDto extends PartialType(CreateMeetingRoomDto) {
+  @ApiProperty()
   @IsNotEmpty({
     message: '会议室ID不能为空',
   })
